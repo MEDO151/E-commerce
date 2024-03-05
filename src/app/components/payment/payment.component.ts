ab285ec125ab5e37f16cdd8cc062df8405c39352
MEDO151 <alaaali2847@gmail.com>
2024-03-06 01:45:48 +0200

    first

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -20,15 +20,11 @@ export class PaymentComponent implements OnInit {
     city: new FormControl('' ,[Validators.required])
   })
 
-  lll:any = ''
 
   ngOnInit(): void {
     this._ActivatedRoute.paramMap.subscribe({
       next: (params) =>{
         this.cartId = params.get('id');
-        this.lll = params.get('baseUrl');
-        console.log(this.cartId, this.lll);
-        console.log(params);
       }
     })
   }
